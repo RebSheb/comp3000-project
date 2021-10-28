1bb289c30af63055f8099f3d3021aa06904bafbb
Rob Wilson <Robert.Wilson@qinetiq.com>
2021-10-28 13:56:26 +0100

    Renamed placeholder function names

--- Update-Agent/base_classes.py
+++ Update-Agent/base_classes.py
@@ -1,11 +1,9 @@
 import logging
 
+
 class UpdateHandler():
     def __init__(self):
         logging.info("UpdatedHandler instantiated")
 
-    def do_update(self):
-        pass
-
-    def something(self):
+    def check_for_updates(self):
         pass
