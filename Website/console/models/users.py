be390218d11741bca9beaa13d243df571a18fcad
Rob Wilson <Robert.Wilson@qinetiq.com>
2021-12-13 16:42:56 +0000

    Added some default properties which don't seem to be getting inherited from UserMixins

--- Website/console/models/users.py
+++ Website/console/models/users.py
@@ -4,18 +4,31 @@ from flask_login import UserMixin
 import datetime
 
 
-class User(db.Model, UserMixin):
+class User(UserMixin, db.Model):
 
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(512), nullable=False)
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    username = db.Column(db.String(32), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
     registered_on = db.Column(db.DateTime, nullable=False)
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
 
     def __init__(self, username, password):
         self.username = username
-        self.password = bcrypt.generate_password_hash(password, 24)
+        self.password = bcrypt.generate_password_hash(password, 12)
         self.registered_on = datetime.datetime.now()
 
     def __repr__(self):
