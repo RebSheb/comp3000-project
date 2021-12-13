f9266db9b800395981304e79a8719bca730fb81c
Rob Wilson <Robert.Wilson@qinetiq.com>
2021-12-13 16:42:14 +0000

    Authenticated py adjustments

--- Website/console/authenticated/authenticated.py
+++ Website/console/authenticated/authenticated.py
@@ -1,12 +1,15 @@
 from flask import render_template, Blueprint
+from flask_login import login_required
 
 auth_bp = Blueprint("authenticated", __name__)
 
 
-@auth_bp.route("/home")
+@auth_bp.route("/")
+@login_required
 def home():
     return render_template("index.jinja2")
 
 @auth_bp.route("/about")
+@login_required
 def about():
     return render_template("about.jinja2")
