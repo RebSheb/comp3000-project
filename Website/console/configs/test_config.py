668f5764126c401aea14dd8d01df051414827aab
Rob Wilson <Robert.Wilson@qinetiq.com>
2021-12-13 14:26:53 +0000

    Remove the SQL_ALCHEMY overhead warning spam

--- Website/console/configs/test_config.py
+++ Website/console/configs/test_config.py
@@ -3,3 +3,4 @@ DEBUG = True
 FLASK_ENV = "development"
 SECRET_KEY = "dklgjsdgiogreg9hry904u09euy09eu9ty43uj9i6uj4w8963wp89g64739p8"
 SQLALCHEMY_DATABASE_URI = "sqlite://db.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
