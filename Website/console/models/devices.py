e0a9c97dd937cd231850d3acada72fd6b81c9059
Rob Wilson <Robert.Wilson@qinetiq.com>
2021-12-14 14:21:21 +0000

    MAC and IP Address

--- Website/console/models/devices.py
+++ Website/console/models/devices.py
@@ -2,5 +2,14 @@ from ..app import db, bcrypt
 import datetime
 
 class Device(db.Model):
-    def __init__(self, mac_address, ip_address,) -> None:
+
+    mac_address = db.Column(db.Integer, primary_key=True)
+    ipv4_address = db.Column(db.String(15), nullable=False)
+
+    def __init__(self, mac_address, ipv4_address) -> None:
+        self.mac_address = mac_address
+        self.ip_address = ipv4_address
         return
+
+    def __repr__(self):
+        return "<Device %r" % self.mac_address
