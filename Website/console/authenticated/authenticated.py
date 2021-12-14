bb2231bb616ecfdeeb4f2a83edeb0c7bcfad424e
Rob Wilson <Robert.Wilson@qinetiq.com>
2021-12-14 10:52:34 +0000

    Refactor / reword terms

--- Website/console/authenticated/authenticated.py
+++ Website/console/authenticated/authenticated.py
@@ -1,15 +1,23 @@
 from flask import render_template, Blueprint
 from flask_login import login_required
+from flask_login.utils import logout_user
+from werkzeug.utils import redirect
 
-auth_bp = Blueprint("authenticated", __name__)
+auth_bp = Blueprint("authenticated", __name__, template_folder="templates")
 
 
 @auth_bp.route("/")
 @login_required
 def home():
-    return render_template("index.jinja2")
+    return render_template("dashboard.jinja2")
 
 @auth_bp.route("/about")
 @login_required
 def about():
     return render_template("about.jinja2")
+
+@auth_bp.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
