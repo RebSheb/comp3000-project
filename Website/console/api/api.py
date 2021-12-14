dc28296789143179130bc01a7b582d4126d02be2
Rob Wilson <Robert.Wilson@qinetiq.com>
2021-12-14 10:52:23 +0000

    API Skeleton

---
+++ Website/console/api/api.py
@@ -0,0 +1,7 @@
+from flask import render_template, Blueprint
+from flask_login import login_required
+from flask_login.utils import logout_user
+from werkzeug.utils import redirect
+
+api = Blueprint("api_bp", __name__)
+
