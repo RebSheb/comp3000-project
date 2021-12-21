eec12e46e2a4e961c68ce51d2077cb1b0755bed9
Rob Wilson <Robert.Wilson@qinetiq.com>
2021-12-21 20:39:38 +0000

    Create some device specific fields

--- Website/console/models/devices.py
+++ Website/console/models/devices.py
@@ -1,10 +1,14 @@
-from ..app import db, bcrypt
+from ..app import db
+from sqlalchemy import DateTime
+from sqlalchemy.
 import datetime
 
 class Device(db.Model):
 
     mac_address = db.Column(db.Integer, primary_key=True)
     ipv4_address = db.Column(db.String(15), nullable=False)
+    last_seen = db.Column(DateTime, default=datetime.datetime.now())
+    first_seen = db.COlumn(DateTime, default=datetime.datetime.now())
 
     def __init__(self, mac_address, ipv4_address) -> None:
         self.mac_address = mac_address
