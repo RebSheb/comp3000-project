47d079a90644e714623bb8dcda9df13eca1da749
Robert.Wilson <robert.wilson@qinetiq.com>
2022-01-04 11:33:36 +0000

    Import DB in prep for devices to be added to the database

--- Website/console/authenticated/authenticated.py
+++ Website/console/authenticated/authenticated.py
@@ -1,11 +1,14 @@
-from flask import render_template, Blueprint, flash, current_app
+from flask import render_template, Blueprint, flash
 from flask_login import login_required
 from flask_login.utils import logout_user
 from werkzeug.utils import redirect
 
 # We need ARP (Address Resolution Protocol) to discover devices on our network
 from scapy.all import ARP, Ether, srp
+# We need socket to resolve hostnames by address and herror for HostnameError
 from socket import gethostbyaddr, herror
+from logging import log
+from console import app, db
 
 auth_bp = Blueprint("authenticated", __name__, template_folder="templates")
 
@@ -15,7 +18,7 @@ auth_bp = Blueprint("authenticated", __name__, template_folder="templates")
 def home():
     clients = []
     try:
-        target_ip = current_app.config["IP_RANGE"]
+        target_ip = app.config["IP_RANGE"]
         arp = ARP(pdst=target_ip)
         ether = Ether(dst="ff:ff:ff:ff:ff:ff")
         packet = ether/arp
@@ -27,11 +30,15 @@ def home():
                 (hostname, alias, ip) = gethostbyaddr(received.psrc)
             except herror:
                 hostname = "Unknown Hostname"
-            finally:
-                clients.append(
-                    {'ip': received.psrc, 'mac': received.hwsrc, "hostname": hostname})
+
+            clients.append(
+                {'ip': received.psrc, 'mac': received.hwsrc, "hostname": hostname})
+
+        if len(clients) == 0:
+            flash("No devices found on network!")
+
     except PermissionError as err:
-        flash("A PermissionError error occurred in LANMan! Is it running ")
+        flash("A PermissionError error occurred in LANMan! Is it running as root or does it have permission?")
 
     return render_template("dashboard.jinja2", devices=clients)
 
