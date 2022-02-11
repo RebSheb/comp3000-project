9ccc3fe5e4f1f64ee68db773d7dbf8d72e38acce
Rob <robxplo98@outlook.com>
2022-02-11 16:49:05 +0000

    Grab windows updates via subprocess and powershell

--- Update-Agent/windows_update_handler.py
+++ Update-Agent/windows_update_handler.py
@@ -1,6 +1,8 @@
 import logging
+from re import sub
 from base_classes import UpdateHandler
-
+import subprocess
+import sys
 
 class WindowsUpdater(UpdateHandler):
     def __init__(self):
@@ -9,3 +11,8 @@ class WindowsUpdater(UpdateHandler):
 
     def check_for_updates(self):
         logging.info("WindowsUpdater-CheckingForUpdates")
+        p = subprocess.Popen(["powershell.exe", "$UpdateSession = New-Object -ComObject Microsoft.Update.Session; $UpdateSearcher = $UpdateSession.CreateupdateSearcher(); $Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0').Updates); $Updates | Select-Object Title"], stdout=sys.stdout)
+        p.communicate()
+        print(p)
+
+
