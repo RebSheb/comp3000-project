8875e7142c9a597cf2a440b4632b1f79f6634b6d
Rob <robxplo98@outlook.com>
2022-02-14 19:06:58 +0000

    API receiving test

--- Website/console/api/api.py
+++ Website/console/api/api.py
@@ -1,7 +1,12 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from flask_login import login_required
 from flask_login.utils import logout_user
 from werkzeug.utils import redirect
+from console import app, db
 
 api_bp = Blueprint("api_bp", __name__)
 
+@app.route("/agent/post_data", methods=["POST"])
+def do_post_data():
+    print(request.json)
+    return '', 200
