f9aef05faa70751e77fe531921082b381c4a0540
Rob <robxplo98@outlook.com>
2022-02-28 16:16:55 +0000

    Adjusted code to utilise a passed in mac_address as opposed to gather on call

--- Update-Agent/base_classes.py
+++ Update-Agent/base_classes.py
@@ -1,8 +1,9 @@
 import logging
-from sys import api_version
 import requests
-from uuid import getnode as get_mac # https://stackoverflow.com/questions/159137/getting-mac-address
+# https://stackoverflow.com/questions/159137/getting-mac-address
+from uuid import getnode as get_mac
 import json
+
 
 class UpdateHandler():
     def __init__(self, api_endpoint, api_port):
@@ -10,28 +11,30 @@ class UpdateHandler():
         self.api_endpoint = api_endpoint
         self.api_port = api_port
 
-
     def check_for_updates(self):
         pass
 
-    def post_data(self, data_to_post : list = None):
+    def post_data(self, mac_address: str = None, data_to_post: list = None):
         if data_to_post is None:
             return ("Data to post is None!", False)
+        if mac_address is None:
+            return ("Mac Address is None!", False)
 
         # Format of the list should be like this for Linux
         # [ {"PkgName": "python3", "PkgVersion": "3.9.7", "PkgLatest": "3.9.8"} ]
         # For Windows
         # [ {"PkgName": "Security Intelligence Update for Microsoft Defender Antivirus - KB2267602 (Version 1.359.191.0)", "PkgLatest": "3.9.8", "PkgDescription": "Blah Blah"} ]
-        mac = get_mac()
-        mac = ':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
+        #mac = get_mac()
+        #mac = ':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
         data = {
-            "mac_address": mac,
+            "mac_address": mac_address,
             "data": data_to_post
         }
         data = json.dumps(data)
         try:
             if len(data_to_post) > 0:
-                requests.post(str(self.api_endpoint) + ":" + str(self.api_port) + "/agent/post_data", json=data)
+                requests.post(str(self.api_endpoint) + ":" +
+                              str(self.api_port) + "/agent/post_data", json=data)
         except Exception as error:
             logging.error(error)
             return (str(error), False)
