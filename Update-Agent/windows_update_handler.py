d3b7be015c850cc5f08d06251c303833d1d66ed2
Rob Wilson <robert.wilson-7@students.plymouth.ac.uk>
2022-03-17 14:41:53 +0000

    Adjusted from Popen to check_output which is similar but negates the stdout/stderr

--- Update-Agent/windows_update_handler.py
+++ Update-Agent/windows_update_handler.py
@@ -14,15 +14,22 @@ class WindowsUpdater(UpdateHandler):
 
     def check_for_updates(self):
         logging.info("WindowsUpdater-CheckingForUpdates")
-        p = subprocess.Popen(
-            ["powershell.exe", "$UpdateSession = New-Object -ComObject Microsoft.Update.Session; $UpdateSearcher = $UpdateSession.CreateupdateSearcher(); $Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0').Updates); $Updates | ForEach-Object -Begin $null -Process { $_.Title ; $_.Description ; $_.KBArticleIDs }"], stdout=subprocess.PIPE, universal_newlines=True)
-        stdout, stderr = p.communicate()
+        #p = subprocess.Popen(
+        #    ["powershell.exe", "$UpdateSession = New-Object -ComObject Microsoft.Update.Session; $UpdateSearcher = $UpdateSession.CreateupdateSearcher(); $Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0').Updates); $Updates | ForEach-Object -Begin $null -Process { $_.Title ; $_.Description ; $_.KBArticleIDs }"], stdout=subprocess.PIPE, universal_newlines=True)
+        eap = subprocess.check_output(
+            ["powershell.exe", "$UpdateSession = New-Object -ComObject Microsoft.Update.Session; $UpdateSearcher = $UpdateSession.CreateupdateSearcher(); $Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0').Updates); $Updates | ForEach-Object -Begin $null -Process { $_.Title ; $_.Description ; $_.KBArticleIDs }"],
+            universal_newlines=True)
+                
+        #stdout, stderr = p.communicate()
         # Remove Windows return carriages and split into a list from \n's
-        stdout = stdout.strip("\r").split("\n")
+        #stdout = stdout.strip("\r").split("\n")
+        eap = eap.strip("\r").split("\n")
         # Remove the erronous newline which Powershell seems to enjoy putting in
-        stdout = stdout[:-1]
+        #stdout = stdout[:-1]
+        eap = eap[:-1]
         # Split every 3rd element so we get a list of lists for each update
-        composite_list = [stdout[x:x+3] for x in range(0, len(stdout), 3)]
+        #composite_list = [stdout[x:x+3] for x in range(0, len(stdout), 3)]
+        composite_list = [eap[x:x+3] for x in range(0, len(eap), 3)]
 
         post_data = []
         for update in composite_list:
@@ -36,3 +43,8 @@ class WindowsUpdater(UpdateHandler):
         mac = ':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
 
         self.post_data(mac, post_data)
+
+    def enumerate_installed_applications(self):
+        eap = subprocess.check_output(["powershell.exe", "$UpdateSession = New-Object -ComObject Microsoft.Update.Session; $UpdateSearcher = $UpdateSession.CreateupdateSearcher(); $Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0').Updates); $Updates | ForEach-Object -Begin $null -Process { $_.Title ; $_.Description ; $_.KBArticleIDs }"], universal_newlines=True)
+
+        return
