eb039f21a954cceda7df20a76c63b24d46eb646e
Rob <robxplo98@outlook.com>
2022-03-30 23:50:05 +0100

    Linux perform_update functionality

--- Update-Agent/linux_update_handler.py
+++ Update-Agent/linux_update_handler.py
@@ -10,22 +10,37 @@ class LinuxUpdater(UpdateHandler):
         logging.info("Linux Updater instantiated")
 
     def check_for_updates(self):
-        upgradable_pkgs = []
-        logging.info("LinuxUpdater-CheckingForUpdates")
+        try:
+            upgradable_pkgs = []
+            logging.info("LinuxUpdater-CheckingForUpdates")
+            cache = apt.Cache()
+            cache.update()
+            cache.open()
+            for pkg in cache:
+                if pkg.is_upgradable:
+                    logging.info("Package [{}] is upgradable from version [{}] to [{}]".format(pkg.name,
+                                                                                               pkg.installed.version,
+                                                                                               pkg.candidate.version))
+                    upgradable_pkgs.append(
+                        {"PkgName": pkg.name, "PkgVersion": pkg.installed.version, "PkgLatest": pkg.candidate.version})
+                else:
+                    if pkg != None and pkg.installed != None:
+                        upgradable_pkgs.append(
+                            {"PkgName": pkg.name, "PkgVersion": pkg.installed.version, "PkgLatest": "None"})
+
+            mac = netifaces.ifaddresses("eth0")[netifaces.AF_LINK][0]["addr"]
+            self.post_data(mac, upgradable_pkgs)
+        except Exception as error:
+            logging.error(
+                "An error occured while attempting to check for updates, is an update in progress?")
+            return
+
+    def perform_update(self):
+        logging.info("LinuxUpdater-PerformUpdate - It's time to update!")
         cache = apt.Cache()
         cache.update()
         cache.open()
-        for pkg in cache:
-            if pkg.is_upgradable:
-                logging.info("Package [{}] is upgradable from version [{}] to [{}]".format(pkg.name,
-                                                                                           pkg.installed.version,
-                                                                                           pkg.candidate.version))
-                upgradable_pkgs.append(
-                    {"PkgName": pkg.name, "PkgVersion": pkg.installed.version, "PkgLatest": pkg.candidate.version})
-            else:
-                if pkg != None and pkg.installed != None:
-                    upgradable_pkgs.append(
-                        {"PkgName": pkg.name, "PkgVersion": pkg.installed.version, "PkgLatest": "None"})
-
-        mac = netifaces.ifaddresses("eth0")[netifaces.AF_LINK][0]["addr"]
-        self.post_data(mac, upgradable_pkgs)
+        cache.upgrade()
+        logging.info("Upgrade finalizing; commiting to cache")
+        cache.commit()
+        logging.info("Upgrade complete")
