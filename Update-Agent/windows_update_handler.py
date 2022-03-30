7adfec1abb586cff6887da2055ca92139158735a
Rob <robxplo98@outlook.com>
2022-03-30 23:51:14 +0100

    Adjusted name from "eap" to "powershell_output"

--- Update-Agent/windows_update_handler.py
+++ Update-Agent/windows_update_handler.py
@@ -16,7 +16,7 @@ class WindowsUpdater(UpdateHandler):
         logging.info("WindowsUpdater-CheckingForUpdates")
         # p = subprocess.Popen(
         #    ["powershell.exe", "$UpdateSession = New-Object -ComObject Microsoft.Update.Session; $UpdateSearcher = $UpdateSession.CreateupdateSearcher(); $Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0').Updates); $Updates | ForEach-Object -Begin $null -Process { $_.Title ; $_.Description ; $_.KBArticleIDs }"], stdout=subprocess.PIPE, universal_newlines=True)
-        eap = subprocess.check_output(
+        powershell_output = subprocess.check_output(
             ["powershell.exe",
                 "$UpdateSession = New-Object -ComObject Microsoft.Update.Session; $UpdateSearcher = $UpdateSession.CreateupdateSearcher(); $Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0').Updates); $Updates | ForEach-Object -Begin $null -Process { $_.Title ; $_.Description ; $_.KBArticleIDs }"],
             universal_newlines=True)
@@ -24,13 +24,14 @@ class WindowsUpdater(UpdateHandler):
         # stdout, stderr = p.communicate()
         # Remove Windows return carriages and split into a list from \n's
         # stdout = stdout.strip("\r").split("\n")
-        eap = eap.strip("\r").split("\n")
+        powershell_output = powershell_output.strip("\r").split("\n")
         # Remove the erronous newline which Powershell seems to enjoy putting in
         # stdout = stdout[:-1]
-        eap = eap[:-1]
+        powershell_output = powershell_output[:-1]
         # Split every 3rd element so we get a list of lists for each update
         # composite_list = [stdout[x:x+3] for x in range(0, len(stdout), 3)]
-        composite_list = [eap[x:x+3] for x in range(0, len(eap), 3)]
+        composite_list = [powershell_output[x:x+3]
+                          for x in range(0, len(powershell_output), 3)]
 
         post_data = []
         for update in composite_list:
