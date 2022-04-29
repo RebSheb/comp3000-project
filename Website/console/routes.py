f0adbd57bcc5f00e74909aac7d7453898e71e0cc
Rob <robxplo98@outlook.com>
2022-04-29 04:44:49 +0100

    Implement a is_active check on login and respond accordingly

--- Website/console/routes.py
+++ Website/console/routes.py
@@ -36,6 +36,11 @@ def do_login():
     try:
         user = User.query.filter_by(username=request.form["username"]).first()
 
+        if not user.is_active:
+            flash(
+                "This user is not yet active, please check with your LANMan administrator...")
+            return redirect(url_for("login"))
+
         if user is None or not bcrypt.check_password_hash(user.password, request.form["password"]):
             flash("Please check your username / password!")
             return redirect(url_for("login"))
