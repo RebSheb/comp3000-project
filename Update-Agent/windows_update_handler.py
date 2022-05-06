64a0a8bd2bc3da3c31d1f20d9c42f6dd22c4da79
Rob <robxplo98@outlook.com>
2022-05-06 09:44:22 +0100

    Multi-line for ease of readability

--- Update-Agent/windows_update_handler.py
+++ Update-Agent/windows_update_handler.py
@@ -16,14 +16,16 @@ class WindowsUpdater(UpdateHandler):
 
     def check_for_updates(self):
         logging.info("WindowsUpdater-CheckingForUpdates")
-        # p = subprocess.Popen(
-        #    ["powershell.exe", "$UpdateSession = New-Object -ComObject Microsoft.Update.Session; $UpdateSearcher = $UpdateSession.CreateupdateSearcher(); $Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0').Updates); $Updates | ForEach-Object -Begin $null -Process { $_.Title ; $_.Description ; $_.KBArticleIDs }"], stdout=subprocess.PIPE, universal_newlines=True)
+
         powershell_output = subprocess.check_output(
             ["powershell.exe",
-                "$UpdateSession = New-Object -ComObject Microsoft.Update.Session; $UpdateSearcher = $UpdateSession.CreateupdateSearcher(); $Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0').Updates); $Updates | ForEach-Object -Begin $null -Process { $_.Title ; $_.Description ; $_.KBArticleIDs }"],
+                """$UpdateSession = New-Object -ComObject Microsoft.Update.Session;
+                 $UpdateSearcher = $UpdateSession.CreateupdateSearcher();
+                 $Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0').Updates);
+                 $Updates | ForEach-Object -Begin $null -Process { $_.Title ; $_.Description ; $_.KBArticleIDs }"""
+             ],
             universal_newlines=True)
 
-        # stdout, stderr = p.communicate()
         # Remove Windows return carriages and split into a list from \n's
         # stdout = stdout.strip("\r").split("\n")
         powershell_output = powershell_output.strip("\r").split("\n")
