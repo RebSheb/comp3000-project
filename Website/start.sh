74fdf3cbc6784876dd0abe3cd1b581fd1b8d0622
Rob <robxplo98@outlook.com>
2022-05-07 17:09:41 +0100

    Website install pre-cursor packages

--- Website/start.sh
+++ Website/start.sh
@@ -1,5 +1,4 @@
-source environment/bin/activate
-
+sudo apt install -y tcpdump python3 python3-requests python3-netifaces python3-apt python3-flask python3-sqlalchemy
 pip3 install -r requirements.txt
 
 export FLASK_APP=console:create_app
